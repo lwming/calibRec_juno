f3f4e74b79d0e4406f813b8f65d84518c688b42b
zhangxt-ihep <zhangxuantong@ihep.ac.cn>
2019-09-24 15:56:45 +0800

    update PMTCalib/

--- PMTCalib/src/PMTCalib.h
+++ PMTCalib/src/PMTCalib.h
@@ -11,45 +11,42 @@
 class ElecFeeCrate;
 class DataBufSvcV2;
 
-namespace JM
-{
-    class ElecFeeCrate;
-    class ElecFeeChannel;
+namespace JM {
+class ElecFeeCrate;
+class ElecFeeChannel;
 }
 
-class PMTCalib: public AlgBase
-{
-    public:
-        PMTCalib(const std::string& name);
-        ~PMTCalib();
+class PMTCalib : public AlgBase {
+  public:
+  PMTCalib(const std::string& name);
+  ~PMTCalib();
 
-    public:
-        bool initialize();
-        bool execute();
-        bool finalize();
+  public:
+  bool initialize();
+  bool execute();
+  bool finalize();
 
-    private:
-        double integral(JM::ElecFeeChannel& channel);
-    private:
+  private:
+  double integral(JM::ElecFeeChannel& channel);
 
-        IDataMemMgr* m_memMgr;
-        std::ofstream output1;
-        JM::ElecFeeCrate* m_crate;
-        std::string m_CalibFile;
-	    TH1D* m_meanWaveform[20000];
-        TH1D* m_SPERE[20000];
-        TH1D* m_SPEIM[20000];
-	    TH1D* m_Integral[20000];
-	    std::map< int, double> m_inte;
-        std::map< int, double> m_intesigma;
-	    int m_length;
-	    int m_step;
-        int m_totalPMT;
-        double m_threshold;
-	    TH1* m_tempH;
-	    double m_stat[20000];
-	    int m_store;
-        double m_refT;
+  private:
+  IDataMemMgr* m_memMgr;
+  std::ofstream output1;
+  JM::ElecFeeCrate* m_crate;
+  std::string m_CalibFile;
+  TH1D* m_meanWaveform[20000];
+  TH1D* m_SPERE[20000];
+  TH1D* m_SPEIM[20000];
+  TH1D* m_Integral[20000];
+  std::map<int, double> m_inte;
+  std::map<int, double> m_intesigma;
+  int m_length;
+  int m_step;
+  int m_totalPMT;
+  double m_threshold;
+  TH1* m_tempH;
+  double m_stat[20000];
+  int m_store;
 };
 
 #endif
